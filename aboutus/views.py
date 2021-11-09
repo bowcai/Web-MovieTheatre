73c1cc21311cbc275ac6a74bc1fac3fb7301c50c
bowcai <caibowen.cbw@outlook.com>
2021-11-10 00:57:32 +0800

    Create the structure of pages

---
+++ aboutus/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+
+
+# Create your views here.
+def index_redirect(request):
+    return HttpResponsePermanentRedirect('/about-us/')
+
+
+def index(request):
+    return HttpResponse('Hello World.')
--- movietheatre/urls.py
+++ movietheatre/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from aboutus import views
 
 urlpatterns = [
+    path('', views.index_redirect),
+    path('about-us/', include('aboutus.urls')),
     path('admin/', admin.site.urls),
 ]
