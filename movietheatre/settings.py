f3247e1c085984db406715b453123060c3477d0b
bowcai <caibowen.cbw@outlook.com>
2021-11-10 12:04:49 +0800

    Set up the html page for "About us"

--- movietheatre/settings.py
+++ movietheatre/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'movietheatre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'aboutus', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
