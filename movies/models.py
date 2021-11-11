6f7c68c68d6ff0820b723b31c2a171ce3c664a1e
bowcai <caibowen.cbw@outlook.com>
2021-11-11 21:53:20 +0800

    Add movie class in database

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    duration_min = models.PositiveSmallIntegerField()
+    grade = models.CharField(max_length=255)
+    poster_url = models.CharField(max_length=8192)
+
+    def __str__(self):
+        return self.name
