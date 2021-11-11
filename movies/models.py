cb3cd590e522d1850e066c1760148079682f653f
BeVWin <448434616@qq.com>
2021-11-11 23:09:42 +0800

    Add a new column for detail URL

--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,7 @@ class Movie(models.Model):
     duration_min = models.PositiveSmallIntegerField()
     grade = models.CharField(max_length=255)
     poster_url = models.CharField(max_length=8192)
+    detail_url = models.CharField(max_length=8192)
 
     def __str__(self):
         return self.name
